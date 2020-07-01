eb91aebfefd19e79333cc515e97056170fa43f75
Cevheri Bozoğlan <cevheri.bozoglan@aurorabilisim.com>
2020-07-01 18:13:04 +0300

    add file and add method input, int(), str(), float()

---
+++ exercise_01/11_input_func.py
@@ -0,0 +1,24 @@
+# input from user
+name = "Cevheri"
+print("Your name is", name)
+
+# opened name = input("your name")
+print("Your name is", name)
+####################
+
+# int() Convert a number or string to an integer
+# opened val1 = input("your age")
+# opened age = int(val1)
+# opened print(age ** 2)
+####################
+
+# str()  Create a new string object from the given object.
+num = 127673642
+# error print(len(num))
+print(len(str(num)))
+#####################
+
+# float()
+v1 = "23"
+type(v1)  # <class 'str'>
+print(float(v1))  # 23.0
