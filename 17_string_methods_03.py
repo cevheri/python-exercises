6572f392f55ef10b5d989de01ad7c406b344b5d5
Cevheri Bozoğlan <cevheri.bozoglan@aurorabilisim.com>
2020-07-09 19:08:12 +0300

    add string method : str.maketrans(), translate(), isalpha(), isdigit(), isalnum(), isdecimal(), isidentifier(), isnumeric(), isspace(), isprintable()

---
+++ 17_string_methods_03.py
@@ -0,0 +1,11 @@
+#  String Methods 03
+#
+#  str.maketrans(), translate(), isalpha(), isdigit(), isalnum(), isdecimal(), isidentifier(), isnumeric(), isspace(), isprintable()
+
+#  str.maketrans(), translate()
+print()
+# str.maketrans(), translate()
+print("str.maketrans(), translate()", "-" * 30, sep="\n")
+c = " pyt hon "
+print(c.maketrans())  # ' ', '\t', '\n', '\r', '\v', '\f'
+print()
