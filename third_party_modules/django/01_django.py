b87376a475f6435c812a74315cdadc69618e7d39
cevher <cevheribozoglan@gmail.com>
2020-08-09 17:34:20 +0300

    django, create first project

---
+++ third_party_modules/django/01_django.py
@@ -0,0 +1,44 @@
+# https://docs.djangoproject.com/en/2.0/intro/tutorial01/
+# https://docs.djangoproject.com/en/3.1/intro/tutorial01/
+
+
+
+# python -m django --version
+#  django-admin startproject mysite
+# mysite/
+#     manage.py
+#     mysite/
+#         __init__.py
+#         settings.py
+#         urls.py
+#         wsgi.py
+#
+# python manage.py runserver
+# python manage.py  runserver 8080
+#
+# Watching for file changes with StatReloader
+# Performing system checks...
+#
+# System check identified no issues (0 silenced).
+#
+# You have 17 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+# Run 'python manage.py migrate' to apply them.
+#
+# August 09, 2020 - 14:17:16
+# Django version 2.2.12, using settings 'mysite.settings'
+# Starting development server at http://127.0.0.1:8000/
+# Quit the server with CONTROL-C.
+#
+# python3 manage.py startapp polls
+# polls/
+#     __init__.py
+#     admin.py
+#     apps.py
+#     migrations/
+#         __init__.py
+#     models.py
+#     tests.py
+#     views.py
+
+# python3 manage.py runserver
+# http://127.0.0.1:8000/polls/
