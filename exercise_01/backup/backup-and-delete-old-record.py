e6b41e0a558fbc71d59e3fc030403445a9a13dfe
Cevheri Bozoğlan <cevheri.bozoglan@aurorabilisim.com>
2020-08-29 16:14:35 +0300

    modify backup programs

--- exercise_01/backup/backup-and-delete-old-record.py
+++ exercise_01/backup/backup-and-delete-old-record.py
@@ -3,12 +3,14 @@
 # cevheri, 20200829
 # Written in Python 3.7
 ###
+import datetime
+import json
+import logging
 import os
 import shutil
-import json
-import datetime
-import logging
 import sys
+
+del_count = 3
 
 
 # read config (source and target folder)
@@ -16,11 +18,15 @@ def read_config():
     try:
         with open("param.json") as config:
             data = json.load(config)
+        src = data["source_folder"]
+        dest = data["target_folder"]
+
+        global del_count
+        del_count = int(data["del_count"])
     except Exception as err:
-        #logging.error("Read Configuration Error:" + " " + err)
-        sys.exit(os.EX_SOFTWARE)
-    src = data["source_folder"]
-    dest = data["target_folder"]
+        logging.error("Read Configuration Error:" + " " + str(type(err)) + ">" + str(err))
+        sys.exit()
+
     add = "\\backup_{}".format(datetime.datetime.today().strftime('%Y_%m_%d_%H_%M_%S'))
     docopy(src, dest + add)
     del_old_record(dest)
@@ -42,7 +48,7 @@ def del_old_record(target_folder):
         # if os.path.isdir(dir):
         path = os.path.join(target_folder, dir)
         count += 1
-        if count > 10:
+        if count > del_count:
             try:
                 shutil.rmtree(path)
                 logging.info("Deleted :> " + path)
